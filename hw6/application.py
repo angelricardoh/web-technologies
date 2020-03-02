a8a74245b370dc23c98d4a928148b48a7756b2bc
Angel Ricardo Nieto Garcia <nietogar@usc.edu>
2020-03-02 01:09:23 -0800

    Add top words

--- hw6/application.py
+++ hw6/application.py
@@ -1,31 +1,48 @@
 from flask import Flask, jsonify, request
 from newsapi import NewsApiClient
-import json
+import os.path
+from os import path
+from flask import current_app
+
 
 app = Flask(__name__)
 newsapi = NewsApiClient(api_key='f020b671fc534b77b9cd0976b0fbdeb8')
-top_headlines = newsapi.get_top_headlines(sources='cnn,fox-news', page_size=30)
+stop_words = []
 
-
-# Init
-
-# # /v2/top-headlines
-# top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                           sources='bbc-news,the-verge',
-#                                           category='business',
-#                                           language='en',
-#                                           country='us')
 
 @app.route('/')
 @app.route('/index')
 def index():
+    with current_app.open_resource('static/stopwords_en.txt') as input_f:
+        for line in input_f.readlines():
+            stop_words.append(line.rstrip().decode("utf-8"))
+        input_f.close()
+
     return app.send_static_file("index.html")
 
 
 @app.route('/news', methods=['GET'])
 def news():
-    print(top_headlines)
-    return jsonify(top_headlines["articles"])
+    try:
+        top_headlines = newsapi.get_top_headlines(sources='cnn,fox-news', page_size=30)
+    except:
+        return "Error server"
+    articles = top_headlines["articles"]
+    titles_words_dict = {}
+    for article in articles:
+        title = article["title"]
+        title_words = title.split()
+        for title_word in title_words:
+            if title_word.lower() in stop_words: continue
+            if title_word in titles_words_dict:
+                titles_words_dict[title_word] += 1
+            else:
+                titles_words_dict[title_word] = 1
+    top_words_list = sorted(titles_words_dict.items(), key=lambda x: x[1], reverse=True)
+    top_words = [a_tuple[0] for a_tuple in top_words_list]
+
+    top_headlines["top_words"] = top_words[0:30]
+    return jsonify(top_headlines)
 
 
 @app.route('/search', methods=['GET'])
@@ -55,9 +72,11 @@ def search():
 
     # jsonify(top_headlines)
     # parse_json = json.loads(top_headlines)
-    result = newsapi.get_everything(q=keyword, sources=sources,
-                                    from_param=from_date, to=to_date, language='en', sort_by='relevancy', page_size=30)
-
+    try:
+        result = newsapi.get_everything(q=keyword, sources=sources,
+                                        from_param=from_date, to=to_date, language='en', sort_by='relevancy', page_size=30)
+    except:
+        return "Error server"
     return jsonify(result)
 
 
--- hw6/static/controller.js
+++ hw6/static/controller.js
@@ -1,13 +1,13 @@
 let base_url = 'http://127.0.0.1:5000/';
 
 window.onload = function() {
-    // xmlhttp = new XMLHttpRequest();
-    // let headlines_request_url = base_url + 'news';
-    // // console.log(headlines_request_url);
-    // xmlhttp.open("GET",headlines_request_url,false);
-    // xmlhttp.send();
-    // var response = JSON.parse(xmlhttp.responseText);
-    // console.log(response);
+    xmlhttp = new XMLHttpRequest();
+    let headlines_request_url = base_url + 'news';
+    // console.log(headlines_request_url);
+    xmlhttp.open("GET",headlines_request_url,false);
+    xmlhttp.send();
+    var response = JSON.parse(xmlhttp.responseText);
+    console.log(response);
     // document.getElementById('headers').innerHTML= response;
 }
 

9ec0ab0896839287b59c0a3ecadfd201523220d4
Angel Ricardo Nieto Garcia <nietogar@usc.edu>
2020-03-04 12:09:53 -0800

    Add top-words layout

--- hw6/static/controller.js
+++ hw6/static/controller.js
@@ -1,4 +1,5 @@
 let base_url = 'http://127.0.0.1:5000/';
+var words_cloud = "";
 
 window.onload = function() {
     xmlhttp = new XMLHttpRequest();
@@ -9,6 +10,12 @@ window.onload = function() {
       if (xmlhttp.readyState === 4) {
         if (xmlhttp.status === 200) {
             jsonObj = JSON.parse(xmlhttp.responseText);
+            top_words = jsonObj.top_words;
+            top_words_array = [];
+            for (top_words_index in top_words) {
+                top_words_array.push(top_words[top_words_index]);
+            }
+            generateWordsCloudLayout(top_words_array);
             let validArticles = getValidArticles(jsonObj.articles);
             generateArticlesLayout(validArticles);
         } else {
@@ -26,7 +33,7 @@ function search() {
     // document.getElementById('headers').innerHTML= "Something else";
 }
 
-function selectMenuOption(evt, cityName) {
+function selectMenuOption(menuOption) {
   // Declare all variables
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -37,7 +44,7 @@ function selectMenuOption(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(menuOption).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
@@ -98,7 +105,7 @@ function generateArticlesLayout(articles) {
     div_news_container.appendChild(cnn_news_container);
     div_news_container.appendChild(fox_news_container);
 
-    document.getElementById("headers").appendChild(div_news_container);
+    document.getElementById("top_news").appendChild(div_news_container);
 }
 
 function createNewsContainer(title, articles) {
@@ -131,4 +138,55 @@ function createNewsContainer(title, articles) {
         news_container.appendChild(card);
     }
     return news_container
+}
+
+function generateWordsCloudLayout(top_words){
+
+var myWords = top_words.map(function(d) {
+      return {word: d, size: 10 + Math.random() * 30};
+    })
+// var myWords = [{word: "Running", size: "10"}, {word: "Surfing", size: "20"}, {word: "Climbing", size: "50"}, {word: "Kiting", size: "30"}, {word: "Sailing", size: "20"}, {word: "Snowboarding", size: "60"} ]
+
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 450 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#word_cloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+// Wordcloud features that are different from one word to the other must be here
+var layout = d3.layout.cloud()
+  .size([width, height])
+  .words(myWords.map(function(d) { return {text: d.word, size:d.size}; }))
+  .padding(5)        //space between words
+  .rotate(function() { return ~~(Math.random() * 2) * 90; })
+  .fontSize(function(d) { return d.size; })      // font size of words
+  .on("end", draw);
+layout.start();
+
+// This function takes the output of 'layout' above and draw the words
+// Wordcloud features that are THE SAME from one word to the other can be here
+function draw(words) {
+  svg
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size; })
+        .style("fill", "#69b3a2")
+        .attr("text-anchor", "middle")
+        .style("font-family", "Impact")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+}
 }

b3ebfe315e180ca3091b88f92ccf2e3b343bc233
Angel Ricardo Nieto Garcia <nietogar@usc.edu>
2020-03-04 00:17:31 -0800

    Add cards and news layout

--- hw6/static/controller.js
+++ hw6/static/controller.js
@@ -8,8 +8,9 @@ window.onload = function() {
     xmlhttp.onload = function (e) {
       if (xmlhttp.readyState === 4) {
         if (xmlhttp.status === 200) {
-            document.getElementById("headers").innerHTML = xmlhttp.responseText;
-          // console.log(xmlhttp.responseText);
+            jsonObj = JSON.parse(xmlhttp.responseText);
+            let validArticles = getValidArticles(jsonObj.articles);
+            generateArticlesLayout(validArticles);
         } else {
           console.error(xmlhttp.statusText);
         }
@@ -19,34 +20,115 @@ window.onload = function() {
         console.error(xhr.statusText);
     };
     xmlhttp.send();
-
-
-    // var response = JSON.parse(xmlhttp.responseText);
-    // console.log(response);
-    // document.getElementById('headers').innerHTML= response;
 }
 
 function search() {
     // document.getElementById('headers').innerHTML= "Something else";
 }
 
-function openCity(evt, cityName) {
+function selectMenuOption(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
-  // for (i = 0; i < tabcontent.length; i++) {
-  //   tabcontent[i].style.display = "none";
-  // }
-
-  // Get all elements with class="tablinks" and remove the class "active"
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
-  // Show the current tab, and add an "active" class to the link that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function getValidArticles(articles) {
+    var valid_articles = [];
+    for (let article_index in articles) {
+        article = articles[article_index];
+        author = article.author;
+        title = article.title;
+        description = article.description;
+        url = article.url;
+        urlToImage = article.urlToImage;
+        publishedAt = article.publishedAt;
+        source = article.source;
+
+        if (author == null || author.length == 0 ||
+            title == null || title.length == 0 ||
+            description == null || description.length == 0 ||
+            url == null || url.length == 0 ||
+            urlToImage == null ||
+            publishedAt == null || publishedAt.length == 0 ||
+            source == null || source.id == null || source.id.length == 0 ||
+            source.name == null || source.name.length == 0)
+        {
+            continue;
+        } else {
+            valid_articles.push(article);
+        }
+    }
+    return valid_articles
+}
+
+function generateArticlesLayout(articles) {
+    var cnn_articles = [];
+    var fox_articles = [];
+
+    for (let article_index in articles) {
+        let article = articles[article_index];
+        if (article.source.id == "fox-news") {
+            if (fox_articles.length < 5) {
+                fox_articles.push(article);
+            }
+        } else if (article.source.id == "cnn") {
+            if (cnn_articles.length < 5) {
+                cnn_articles.push(article);
+            }
+        }
+        if (fox_articles.length == 5 && cnn_articles.length == 5) {
+            break;
+        }
+    }
+
+    var div_news_container = document.createElement("div");
+    var top_headline_container = document.createElement("div");
+    let cnn_news_container = createNewsContainer("CNN", cnn_articles);
+    let fox_news_container = createNewsContainer("Fox News", fox_articles);
+
+    div_news_container.appendChild(cnn_news_container);
+    div_news_container.appendChild(fox_news_container);
+
+    document.getElementById("headers").appendChild(div_news_container);
+}
+
+function createNewsContainer(title, articles) {
+    var news_container = document.createElement("div");
+    news_container.classList.add("news_container");
+
+    var title_element = document.createElement("p");
+    title_element.textContent = title;
+    var hr = document.createElement("hr");
+
+    news_container.appendChild(title_element);
+    news_container.appendChild(hr);
+
+    for (let article_index in articles) {
+        let article = articles[article_index];
+        var card = document.createElement("div");
+        card.classList.add("card");
+        var image = document.createElement("img");
+        image.src = article.urlToImage;
+        var title_headline = document.createElement("p");
+        var content_headline = document.createElement("p");
+        title_headline.innerText = article.title;
+        content_headline.innerText = article.description;
+        console.log("title: " + title_headline);
+        console.log("content: " + content_headline);
+
+        card.appendChild(image);
+        card.appendChild(title_headline);
+        card.appendChild(content_headline);
+        news_container.appendChild(card);
+    }
+    return news_container
 }

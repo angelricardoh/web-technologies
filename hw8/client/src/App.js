934fc98a2122ec3521cf41638311714d3fb89678
Angel Ricardo Nieto Garcia <angelricardoh@gmail.com>
2020-03-27 17:43:01 -0600

    Add client application and gitignore node modules

---
+++ hw8/client/src/App.js
@@ -0,0 +1,154 @@
+import React from "react";
+import {
+  Link,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import "./App.css";
+
+const users = [
+  {
+    name: `Param`
+  },
+  {
+    name: `Vennila`
+  },
+  {
+    name: `Afrin`
+  }
+];
+
+const IndexPage = () => {
+  return <h3>Home Page</h3>;
+};
+
+const AboutPage = () => {
+  return <h3>About Page</h3>;
+};
+
+const UsersPage = () => {
+  return (
+    <>
+      {users.map((user, index) => (
+        <h5 key={index}>
+          <Link to={`/user/${index + 1}`}>{user.name}'s Page</Link>
+        </h5>
+      ))}
+    </>
+  );
+};
+
+const UserPage = ({ match, location }) => {
+  const {
+    params: { userId }
+  } = match;
+
+  return (
+    <>
+      <p>
+        <strong>User ID: </strong>
+        {userId}
+      </p>
+      <p>
+        <strong>User Name: </strong>
+        {users[userId - 1].name}
+      </p>
+    </>
+  );
+};
+
+const SearchPage = ({ match, location }) => {
+  return (
+    <p>
+      <strong>Query Params: </strong>
+      {location.search}
+    </p>
+  );
+};
+
+const NoMatchPage = () => {
+  return <h3>404 - Not found</h3>;
+};
+
+const PropsPage = ({ title }) => {
+  return <h3>{title}</h3>;
+};
+
+const RedirectPage = () => {
+  return <h3>Redirect Page</h3>;
+};
+
+const AuthPage = ({ isLoggedIn }) => {
+  if (isLoggedIn) {
+    return <Redirect to="/dashboard" />;
+  } else {
+    return <h3>User not loggedin</h3>;
+  }
+};
+
+const DashboardPage = () => {
+  return <h3>Dashboard Page</h3>;
+};
+
+const App = () => {
+  return (
+    <section className="App">
+      <Router>
+        <div style={{ marginBottom: "10px" }}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/users">Users</Link>
+          <Link to="/search?q=react">Search</Link>
+          <Link to="/404-not-found">404</Link>
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+          <Link to="/props-through-component">Props through component</Link>
+          <Link to="/props-through-render">Props through render</Link>
+        </div>
+        <div style={{ marginBottom: "10px" }}>
+          <Link to="/old-route">Redirecting to New page</Link>
+          <Link to="/auth-not-loggedin">Not Loggedin</Link>
+          <Link to="/auth-loggedin">User Loggedin</Link>
+        </div>
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+          <Route exact path="/users" component={UsersPage} />
+          <Route exact path="/user/:userId" component={UserPage} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/search" component={SearchPage} />
+          <Route
+            exact
+            path="/props-through-component"
+            component={() => <PropsPage title={`Props through component`} />}
+          />
+          <Route
+            exact
+            path="/props-through-render"
+            render={props => (
+              <PropsPage {...props} title={`Props through render`} />
+            )}
+          />
+          <Redirect from="/old-route" to="/new-route" />
+          <Route exact path="/new-route" component={RedirectPage} />
+          <Route exact path="/dashboard" component={DashboardPage} />
+          <Route
+            exact
+            path="/auth-not-loggedin"
+            render={props => <AuthPage {...props} isLoggedIn={false} />}
+          />
+          <Route
+            exact
+            path="/auth-loggedin"
+            render={props => <AuthPage {...props} isLoggedIn={true} />}
+          />
+          <Route component={NoMatchPage} />
+        </Switch>
+      </Router>
+      <a href="/about">about with browser reload</a>
+    </section>
+  );
+};
+
+export default App;
--- hw8/server/application.js
+++ hw8/server/application.js
@@ -1,54 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var hike = require('./routes/hike');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-
-var application = express();
-application.get('/hikes', hike.index);
-application.post('/add_hike', hike.add_hike);
-
-// view engine setup
-application.set('views', path.join(__dirname, 'views'));
-application.set('view engine', 'jade');
-
-application.use(logger('dev'));
-application.use(express.json());
-application.use(express.urlencoded({ extended: false }));
-application.use(cookieParser());
-// application.use(express.static(path.join(__dirname, 'public')));
-
-application.use('/', indexRouter);
-application.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-application.use(function(req, res, next) {
-  next(createError(404));
+const express = require('express');
+// const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// error handler
-application.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-exports.index = function(req, res) {
-  res.render('hike', {title: 'My Hiking Log'});
-};
-
-exports.add_hike = function(req, res) {
-};
-
-module.exports = application;
+app.listen(port);

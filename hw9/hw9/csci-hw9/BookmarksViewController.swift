c4074d0f842d8bc0c779af71d5d40bf6589328d3
Angel Ricardo Nieto Garcia <nietogar@usc.edu>
2020-04-19 16:38:41 -0500

    Add hw9

---
+++ hw9/hw9/csci-hw9/BookmarksViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookmarksViewController.swift
+//  csci-hw9
+//
+//  Created by Angel Ricardo Nieto Garcia on 4/19/20.
+//  Copyright © 2020 Angel Ricardo Nieto Garcia. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ hw9/hw9/csci-hw9/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  csci-hw9
+//
+//  Created by Angel Ricardo Nieto Garcia on 4/19/20.
+//  Copyright © 2020 Angel Ricardo Nieto Garcia. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

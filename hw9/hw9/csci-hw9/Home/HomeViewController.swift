db98a2a204f62131ae353af14be4eb1b81e4cfe3
Angel Ricardo Nieto Garcia <angelricardoh@hotmail.com>
2020-05-02 13:00:29 -0500

    Fix scrolling and header

--- hw9/hw9/csci-hw9/Home/HomeViewController.swift
+++ hw9/hw9/csci-hw9/Home/HomeViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import SDWebImage
 import CoreLocation
 
-class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate {
-
-    @IBOutlet weak var newsTableView: UITableView!
+class HomeViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var articles: [Article] = []
     let worker: NewsHomeWorker = NewsHomeWorker()
+    let searchController = UISearchController(searchResultsController: nil)
     
     func locationManager(_ manager: CLLocationManager,  didUpdateLocations locations: [CLLocation]) {
         let lastLocation = locations.last!
@@ -23,15 +24,21 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         print(lastLocation)
     }
 
+    @IBOutlet weak var newsTableView: IntrinsicTableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        refreshControl = UIRefreshControl()
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
         
         worker.fetchNewsHomeInformation(articlesCompletion: {(completion) in
             switch completion {
             case .success(let articles):
                 self.articles = articles
-                self.newsTableView.reloadData()
+                self.tableView.reloadData()
             case .failure(let error):
                 let alertController = UIAlertController(title: "Network Error", message:
                     error.localizedDescription, preferredStyle: .alert)
@@ -41,12 +48,12 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
         })
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // show all rows depending on how many videos we receive
         return articles.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // customize and show data based on array
         let customCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? NewsTableViewCell
         
---
+++ hw9/hw9/csci-hw9/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  csci-hw9
+//
+//  Created by Angel Ricardo Nieto Garcia on 4/19/20.
+//  Copyright © 2020 Angel Ricardo Nieto Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

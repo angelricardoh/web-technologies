63e0eb6ec61f620e33956ea82852eb22c22c228e
Angel Ricardo Nieto Garcia <angelricardoh@hotmail.com>
2020-05-04 21:35:40 -0500

    Fix style in charts section

--- hw9/hw9/csci-hw9/Trending/TrendingViewController.swift
+++ hw9/hw9/csci-hw9/Trending/TrendingViewController.swift
@@ -20,10 +20,7 @@ class TrendingViewController: UIViewController, UITextFieldDelegate, ChartViewDe
         let BarButtonItemAppearance = UIBarButtonItem.appearance()
         BarButtonItemAppearance.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .normal)
         navigationController?.navigationBar.prefersLargeTitles = true
-        title = "Headlines"
-        
-        self.title = "Line Chart 1"
-        
+                
         chartView.delegate = self
         
         chartView.chartDescription?.enabled = false
@@ -31,53 +28,31 @@ class TrendingViewController: UIViewController, UITextFieldDelegate, ChartViewDe
         chartView.setScaleEnabled(true)
         chartView.pinchZoomEnabled = true
         
-        // x-axis limit line
-        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
-        llXAxis.lineWidth = 4
-        llXAxis.lineDashLengths = [10, 10, 0]
-        llXAxis.labelPosition = .bottomRight
-        llXAxis.valueFont = .systemFont(ofSize: 10)
-        
-        chartView.xAxis.gridLineDashLengths = [10, 10]
-        chartView.xAxis.gridLineDashPhase = 0
-        
-        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
-        ll1.lineWidth = 4
-        ll1.lineDashLengths = [5, 5]
-        ll1.labelPosition = .topRight
-        ll1.valueFont = .systemFont(ofSize: 10)
-        
-        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
-        ll2.lineWidth = 4
-        ll2.lineDashLengths = [5,5]
-        ll2.labelPosition = .bottomRight
-        ll2.valueFont = .systemFont(ofSize: 10)
-        
         let leftAxis = chartView.leftAxis
         leftAxis.removeAllLimitLines()
-        leftAxis.addLimitLine(ll1)
-        leftAxis.addLimitLine(ll2)
-        leftAxis.axisMaximum = 200
-        leftAxis.axisMinimum = -50
-        leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.axisMaximum = 110
+        leftAxis.axisMinimum = -10
         leftAxis.drawLimitLinesBehindDataEnabled = true
         
         chartView.rightAxis.enabled = false
-        
-        //[_chartView.viewPortHandler setMaximumScaleY: 2.f];
-        //[_chartView.viewPortHandler setMaximumScaleX: 2.f];
-
-//        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
-//                                   font: .systemFont(ofSize: 12),
-//                                   textColor: .white,
-//                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
-//        marker.chartView = chartView
-//        marker.minimumSize = CGSize(width: 80, height: 40)
-//        chartView.marker = marker
-        
         chartView.legend.form = .line
         
-//        setDataCount(44, range: 100)
+        self.fetchChartData()
+    }
+    
+    func fetchChartData(query: String = "Coronavirus") {
+        trendsWorker.fetchTrends(query: query, trendsCompletion: {(completion) in
+            switch completion {
+            case .success(let trends):
+                self.setDataChart(title:query, data: trends)
+                print(trends)
+            case .failure(let error):
+                let alertController = UIAlertController(title: "Network Error", message:
+                    error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+                self.present(alertController, animated: true, completion: nil)
+            }
+        })
     }
     
     func setDataChart(title: String, data: [Trend]) {
@@ -86,30 +61,14 @@ class TrendingViewController: UIViewController, UITextFieldDelegate, ChartViewDe
                            y: Double($0.y))
         }
             
-        let set1 = LineChartDataSet(entries: values, label: title)
-        set1.drawIconsEnabled = false
+        let linearSet = LineChartDataSet(entries: values, label: "Trending Chart for " + title)
+        linearSet.setColor(.systemBlue)
+        linearSet.setCircleColor(.systemBlue)
+        linearSet.lineWidth = 1
+        linearSet.circleRadius = 3
+        linearSet.drawCircleHoleEnabled = false
         
-        set1.lineDashLengths = [5, 2.5]
-        set1.highlightLineDashLengths = [5, 2.5]
-        set1.setColor(.black)
-        set1.setCircleColor(.black)
-        set1.lineWidth = 1
-        set1.circleRadius = 3
-        set1.drawCircleHoleEnabled = false
-        set1.valueFont = .systemFont(ofSize: 9)
-        set1.formLineDashLengths = [5, 2.5]
-        set1.formLineWidth = 1
-        set1.formSize = 15
-        
-        let gradientColors = [ChartColorTemplates.colorFromString("#00ff0000").cgColor,
-                              ChartColorTemplates.colorFromString("#ffff0000").cgColor]
-        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
-        
-        set1.fillAlpha = 1
-        set1.fill = Fill(linearGradient: gradient, angle: 90) //.linearGradient(gradient, angle: 90)
-        set1.drawFilledEnabled = true
-        
-        let data = LineChartData(dataSet: set1)
+        let data = LineChartData(dataSet: linearSet)
         
         chartView.data = data
     }
@@ -123,18 +82,7 @@ class TrendingViewController: UIViewController, UITextFieldDelegate, ChartViewDe
             return true
         }
         
-        trendsWorker.fetchTrends(query: query, trendsCompletion: {(completion) in
-            switch completion {
-            case .success(let trends):
-                self.setDataChart(title:query, data: trends)
-                print(trends)
-            case .failure(let error):
-                let alertController = UIAlertController(title: "Network Error", message:
-                    error.localizedDescription, preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
-                self.present(alertController, animated: true, completion: nil)
-            }
-        })
+        self.fetchChartData(query: query)
         return true
     }
 }
